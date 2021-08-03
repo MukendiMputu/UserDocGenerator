f6e7cc2bf2c894a52cc01c9fab3f5dc1f971a72f
MukendiMputu <mukendi_mputu@outlook.com>
2021-08-03 02:10:10 +0200

    creates main.java to remove errors

---
+++ src/main/java/info/scce/cinco/product/userdocgenerator/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package info.scce.cinco.product.userdocgenerator.main;
+
+/**
+ * @author mukendi
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
